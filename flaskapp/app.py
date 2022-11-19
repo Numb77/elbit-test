1e4deb97e7ced4cd6957f790e4e5afa113cfd965
Amit Miron <amitmiron77@gmail.com>
2022-11-19 06:45:54 -0800

    final

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return 'Hello Elbit'
 
+@app.route('/proxy')
+def proxyPass():
+    return 'You arrived here via Nginx Proxy'
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
